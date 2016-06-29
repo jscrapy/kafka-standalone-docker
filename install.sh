2b30e489b3f05f8a149aa0938a18363f3ffdbc1e
Mateusz Parzonka <parzonka@gmail.com>
2016-06-29 16:43:18 +0200

    Upgrade kafka to 0.10.0.0

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-wget -O - http://www-eu.apache.org/dist/kafka/0.9.0.1/kafka_2.11-0.9.0.1.tgz  | tar xzf - && mv kafka_2.11-0.9.0.1 /opt/kafka
-
+wget -O - http://www-eu.apache.org/dist/kafka/0.10.0.0/kafka_2.11-0.10.0.0.tgz  | tar xzf - && mv kafka_2.11-0.10.0.0 /opt/kafka
