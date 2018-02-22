753ab50dc31177f4df6924d4e6d86d8fdd088e98
Jon <xuchaoo@gmail.com>
2018-02-22 16:26:26 +0800

    update kafka version to 2.12-1.0.0 and change kafka log.dirs to /data/kafka-logs

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-wget -O - http://www-eu.apache.org/dist/kafka/0.10.0.0/kafka_2.11-0.10.0.0.tgz  | tar xzf - && mv kafka_2.11-0.10.0.0 /opt/kafka
+wget -O http://mirror.bit.edu.cn/apache/kafka/1.0.0/kafka_2.12-1.0.0.tgz 
+tar xzf kafka_2.12-1.0.0.tgz 
+# modify kafka log dir in /opt/kafka/config/server.properties
+sed -i "s/tmp/data/g"  kafka_2.12-1.0.0/config/server.properties
+mv kafka_2.12-1.0.0  /opt/kafka
+
+ 
+
