ba6b9c46fdb35e788b63b8e68ee25ba7baa55537
Jon <xuchaoo@gmail.com>
2018-02-22 16:42:45 +0800

    wget error

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -O http://mirror.bit.edu.cn/apache/kafka/1.0.0/kafka_2.12-1.0.0.tgz 
+wget http://mirror.bit.edu.cn/apache/kafka/1.0.0/kafka_2.12-1.0.0.tgz -O kafka_2.12-1.0.0.tgz 
 tar xzf kafka_2.12-1.0.0.tgz 
 # modify kafka log dir in /opt/kafka/config/server.properties
 sed -i "s/tmp/data/g"  kafka_2.12-1.0.0/config/server.properties
