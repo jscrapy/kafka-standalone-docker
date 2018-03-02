d0213a5de741682ba04fe12a51bdee99481ccea9
Jon <xu_chao@chainatelecom.cn>
2018-03-02 10:02:22 +0800

    fix bug

--- install.sh
+++ install.sh
@@ -8,10 +8,10 @@ tar xzf ${KAFKA}.tgz
 sed -i "s/tmp/data\/kafka/g"  ${KAFKA}/config/server.properties
 
 # keep logs in recent 1 hour
-sed -i "s/168/1/g"  ${KAFKA}/config/zookeeper.properties
+sed -i "s/168/1/g"  ${KAFKA}/config/server.properties
 
 # modify kafka log4j dir
-sed -i "s/LOG_DIR=\"$base_dir\/logs\"/LOG_DIR=\"\/data\/kafka\/logs\"/g"   ${KAFKA}/bin/kafka-run-class.sh
+sed -i "s/\$base_dir\/logs/\/data\/kafka\/logs/g"   ${KAFKA}/bin/kafka-run-class.sh
 # modify zookeeper data dir
 sed -i "s/tmp/data\/kafka\/log4j-logs/g"  ${KAFKA}/config/zookeeper.properties
 
